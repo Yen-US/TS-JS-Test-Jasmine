cbbe46de3925e9e1bdf1c5ba9404ca4fab72951d
Yenson Umaña Solano <yen21000@gmail.com>
2021-09-05 10:47:26 -0600

    Comments added to the code

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,9 +1,11 @@
+//Clase calculator con dos metodos Sum y Difference
 export default class calculator {
+  //Metodo Sum retorna la suma de los dos parametros recibidos
   static Sum(a: number, b: number): number {
     let c = a + b;
     return c;
   }
-
+  // Metodo Difference retorna la resta de los dos parametros recibidos
   static Difference(a: number, b: number): number {
     let c = a - b;
     return c;
--- tests/Calculator.test.ts
+++ tests/Calculator.test.ts
@@ -1,13 +1,18 @@
 import Calculator from '../src/Calculator';
 
+//Define una suite de tests, es decir, una colección de tests. Ésta función recibe dos parámetros, 
+//un string con el nombre de la suite en este caso ‘calculate’ y una función donde definiremos los tests.
 describe('calculate', function() {
+  //Define un test en particular. Recibe cómo parámetro el nombre (‘add’) del test y una función a ejecutar por el test.
   it('add', function() {
     let result = Calculator.Sum(5, 2);
+    //Lo que espera recibir el test. Es decir, con expect hacemos la comprobación del test. Si la comprobación no es cierta el test falla
     expect(result).toBe(7);
   });
-
+  //Define un test en particular. Recibe cómo parámetro el nombre (‘add’) del test y una función a ejecutar por el test.
   it('substract', function() {
     let result = Calculator.Difference(5, 2);
+    //Lo que espera recibir el test. Es decir, con expect hacemos la comprobación del test. Si la comprobación no es cierta el test falla
     expect(result).toBe(3);
   });
 });
